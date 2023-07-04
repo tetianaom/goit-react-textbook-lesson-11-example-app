b49e0d6ebb920c6e1c28414d3d426934bcf7e5af
Tetiana Omanashvili <tanya.nvt@gmail.com>
2023-07-04 21:09:10 +0300

    commit

---
+++ src/components/Task/Task.js
@@ -0,0 +1,26 @@
+import { useDispatch } from 'react-redux';
+import { deleteTask, toggleCompleted } from 'redux/actions';
+import { MdClose } from 'react-icons/md';
+import css from './Task.module.css';
+
+export const Task = ({ task }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => dispatch(deleteTask(task.id));
+  const handleToggle = () => dispatch(toggleCompleted(task.id));
+
+  return (
+    <div className={css.wrapper}>
+      <input
+        type="checkbox"
+        className={css.checkbox}
+        onChange={handleToggle}
+        checked={task.completed}
+      />
+      <p className={css.text}>{task.text}</p>
+      <button type="button" className={css.btn} onClick={handleDelete}>
+        <MdClose size={24} />
+      </button>
+    </div>
+  );
+};
---
+++ src/redux/reducer.js
@@ -0,0 +1,58 @@
+import { statusFilters } from './constants';
+import { combineReducers } from 'redux';
+
+const taskInitialState = [
+  { id: 0, text: 'Learn HTML and CSS', completed: true },
+  { id: 1, text: 'Get good at JavaScript', completed: true },
+  { id: 2, text: 'Master React', completed: false },
+  { id: 3, text: 'Discover Redux', completed: false },
+  { id: 4, text: 'Build amazing apps', completed: false },
+];
+
+// Используем initialState как значение состояния по умолчанию
+export const tasksReducer = (state = taskInitialState, action) => {
+  // Редюсер различает экшены по значению свойства type
+  switch (action.type) {
+    // В зависимости от типа экшена будет выполняться разная логика
+    case 'tasks/addTask':
+      return [...state, action.payload];
+    case 'tasks/deleteTask':
+      return state.filter(task => task.id !== action.payload);
+
+    case 'tasks/toggleCompleted':
+      return state.map(task => {
+        if (task.id !== action.payload) {
+          return task;
+        }
+        return { ...task, completed: !task.completed };
+      });
+
+    default:
+      // Каждый редюсер получает все экшены отправленные в стор.
+      // Если редюсер не должен обрабатывать какой-то тип экшена,
+      // необходимо вернуть существующее состояние без изменений.
+      return state;
+  }
+};
+
+const filtersInitialState = {
+  status: statusFilters.all,
+};
+// Отвечает только за обновление свойства filters
+// Теперь значением параметра state будет объект фильтров
+const filtersReducer = (state = filtersInitialState, action) => {
+  switch (action.type) {
+    case 'filters/setStatusFilter':
+      return {
+        ...state,
+        status: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const rootReducer = combineReducers({
+  tasks: tasksReducer,
+  filters: filtersReducer,
+});
